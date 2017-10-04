6e1c3c2d585b8fbb0062e56d9652f4002b28d793
Jesse Tomchak <jtomchak@gmail.com>
2017-10-04 11:33:55 -0700

    added mainSection with hard coded todo list

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import MainSection from "./MainSection";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <MainSection />
     </div>
   );
 };
---
+++ src/components/MainSection.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class MainSection extends Component {
+  render() {
+    return (
+      <section className="main">
+        <ul className="todo-list">
+          <li>Learn Redux</li>
+        </ul>
+      </section>
+    );
+  }
+}
+
+export default MainSection;
