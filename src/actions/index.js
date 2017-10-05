61b0a2c5166080cb962fdb19ae24374ef348c5ad
Jesse Tomchak <jtomchak@gmail.com>
2017-10-05 16:11:26 -0700

    Delete Todo and Edit Todo

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,3 +9,14 @@ export const completeTodo = id => ({
   type: types.COMPLETE_TODO,
   id
 });
+
+export const deleteTodo = id => ({
+  type: types.DELETE_TODO,
+  id
+});
+
+export const editTodo = (id, text) => ({
+  type: types.EDIT_TODO,
+  id,
+  text
+});
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
 import classnames from "classnames";
+import { deleteTodo } from "../actions/index";
+import TodoTextInput from "./TodoTextInput";
 
 class TodoItem extends Component {
+  state = {
+    editing: false
+  };
+
+  handleDoubleClick = () => {
+    this.setState({ editing: true });
+  };
+
+  handleOnSave = (id, text) => {
+    if (text.length === 0) {
+      this.props.deleteTodo(id);
+    } else {
+      this.props.editTodo(id, text);
+    }
+    this.setState({ editing: false });
+  };
+
   render() {
-    const { todo, completeTodo } = this.props;
-    return (
-      <li
-        className={classnames({
-          completed: todo.completed
-        })}
-      >
+    const { todo, completeTodo, deleteTodo } = this.props;
+    let element;
+    if (this.state.editing) {
+      element = (
+        <TodoTextInput
+          text={todo.text}
+          onSave={text => this.handleOnSave(todo.id, text)}
+        />
+      );
+    } else {
+      element = (
         <div className="view">
           <input
             className="toggle"
@@ -17,8 +40,19 @@ class TodoItem extends Component {
             checked={todo.completed}
             onChange={() => completeTodo(todo.id)}
           />
-          <label>{todo.text}</label>
+          <label onDoubleClick={this.handleDoubleClick}>{todo.text}</label>
+          <button className="destroy" onClick={() => deleteTodo(todo.id)} />
         </div>
+      );
+    }
+
+    return (
+      <li
+        className={classnames({
+          completed: todo.completed
+        })}
+      >
+        {element}
       </li>
     );
   }
